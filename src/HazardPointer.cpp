a50ae055f105528e54112c084bd85b2f7663e524
mjshakir <maslove23@gmail.com>
2024-09-06 01:19:29 -0700

    Remove: code since template is used

--- src/HazardPointer.cpp
+++ src/HazardPointer.cpp
@@ -1,30 +1,5 @@
 //--------------------------------------------------------------
 // Main Header 
 //--------------------------------------------------------------
-#include "HazardSystem/HazardPointer.hpp"
-//--------------------------------------------------------------
-// Standard cpp library
-//--------------------------------------------------------------
-#include <thread>
-//--------------------------------------------------------------
-CircularBuffer::HazardSystem::HazardPointer::HazardPointer(void) : pointer(nullptr) {
-    //--------------------------
-}// end HazardPointer(void)
-//--------------------------------------------------------------
-CircularBuffer::HazardSystem::HazardPointer::HazardPointer(HazardPointer&& other) noexcept 
-                : pointer(other.pointer.exchange(nullptr)) {
-    //--------------------------
-} // end HazardPointer(HazardPointer&& other) noexcept 
-//--------------------------------------------------------------
-// Move assignment operator
-CircularBuffer::HazardSystem::HazardPointer& CircularBuffer::HazardSystem::HazardPointer::operator=(HazardPointer&& other) noexcept {
-    //--------------------------
-    if(this == &other) {
-        return *this;
-    }
-    //--------------------------
-    pointer.store(other.pointer.exchange(nullptr));
-    return *this;
-    //--------------------------
-} // end HazardPointer& operator=(HazardPointer&& other) noexcept
+#include "HazardPointer.hpp"
 //--------------------------------------------------------------
