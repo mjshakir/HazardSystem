9f065cfa3f2331c981bf14a6c0f27d33a2b1cb2d
mjshakir <maslove23@gmail.com>
2025-03-04 20:10:21 -0800

    Add: an error print

--- src/HazardThreadManager.cpp
+++ src/HazardThreadManager.cpp
@@ -3,6 +3,10 @@
 //--------------------------------------------------------------
 #include "HazardThreadManager.hpp"
 //--------------------------------------------------------------
+// Standard cpp library
+//--------------------------------------------------------------
+#include <iostream>
+//--------------------------------------------------------------
 // User Defined Headers
 //--------------------------------------------------------------
 #include "ThreadRegistry.hpp"
@@ -13,10 +17,14 @@ HazardSystem::HazardThreadManager& HazardSystem::HazardThreadManager::instance(v
 }// end HazardSystem::HazardThreadManager::instance(void)
 //--------------------------------------------------------------
 HazardSystem::HazardThreadManager::HazardThreadManager(void) {
-    ThreadRegistry::instance().register_id();
+    if(!ThreadRegistry::instance().register_id()) {
+        std::cerr << "Failed to register thread with ThreadRegistry" << std::endl;
+    }// end if(!ThreadRegistry::instance().register_id())
 }// end HazardSystem::HazardThreadManager(void)
 //--------------------------------------------------------------
 HazardSystem::HazardThreadManager::~HazardThreadManager(void) {
-    ThreadRegistry::instance().unregister();
+    if(!ThreadRegistry::instance().unregister()) {
+        std::cerr << "Failed to unregister thread with ThreadRegistry" << std::endl;
+    }// end if(!ThreadRegistry::instance().unregister())
 }// end ~HazardSystem::HazardThreadManager(void)
 //--------------------------------------------------------------
