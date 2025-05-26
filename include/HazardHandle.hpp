6f2942735dc5c15ad7407b1c3a2689d719457df9
mjshakir <26232377+mjshakir@users.noreply.github.com>
2025-05-25 23:26:30 -0700

    Add: minor comments

--- include/HazardHandle.hpp
+++ include/HazardHandle.hpp
@@ -7,33 +7,38 @@
 #include <type_traits>
 #include <concepts>
 //--------------------------------------------------------------
-template <typename T>
-concept NonFloatArithmetic = std::is_arithmetic_v<T> and !std::is_floating_point_v<T>;
-
-template <NonFloatArithmetic Index, typename T>
-struct HazardHandle {
-    HazardHandle(void) = default;
-
-    HazardHandle(const Index& index_, std::shared_ptr<T> p_data_) : index(index_), sp_data(std::move(p_data_)) {
-
-    }
-
-    HazardHandle(const std::optional<Index>& index_, std::shared_ptr<T> p_data_) : index(index_), sp_data(std::move(p_data_)) {
-
-    }
-
-    ~HazardHandle(void) = default;
-    
-    bool valid(void) const {
-        return sp_data and index.has_value();
-    }
-    void reset(void) {
-        index = std::nullopt;
-        sp_data = nullptr;
-    }
-
-    std::optional<Index> index;
-    std::shared_ptr<T> sp_data;
-    
-};
+namespace HazardSystem {
+    //--------------------------------------------------------------
+    template <typename T>
+    concept NonFloatArithmetic = std::is_arithmetic_v<T> and !std::is_floating_point_v<T>;
+    //--------------------------------------------------------------
+    template <NonFloatArithmetic Index, typename T>
+    struct HazardHandle {
+        //--------------------------------------------------------------
+        HazardHandle(void)  = default;
+        ~HazardHandle(void) = default;
+        //--------------------------
+        HazardHandle(const Index& index_, std::shared_ptr<T> p_data_) : index(index_), sp_data(std::move(p_data_)) {
+            //--------------------------
+        }// end HazardHandle(const Index& index_, std::shared_ptr<T> p_data_)
+        //--------------------------
+        HazardHandle(const std::optional<Index>& index_, std::shared_ptr<T> p_data_) : index(index_), sp_data(std::move(p_data_)) {
+            //--------------------------
+        }// end HazardHandle(const std::optional<Index>& index_, std::shared_ptr<T> p_data_)
+        //--------------------------
+        bool valid(void) const {
+            return sp_data and index.has_value();
+        }// end bool valid(void) cons
+        //--------------------------
+        void reset(void) {
+            index = std::nullopt;
+            sp_data = nullptr;
+        }// end void reset(void)
+        //--------------------------
+        std::optional<Index> index;
+        std::shared_ptr<T> sp_data;
+        //--------------------------
+    };// end struct HazardHandle
+    //--------------------------------------------------------------
+} // end namespace HazardSystem
 //--------------------------------------------------------------
