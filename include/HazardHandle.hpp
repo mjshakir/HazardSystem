8a23e4d6c0f5c380b054d1c23ce1556cc46e5216
mjshakir <26232377+mjshakir@users.noreply.github.com>
2025-06-09 14:51:45 -0700

    Add: copy and move constructor and opterator

--- include/HazardHandle.hpp
+++ include/HazardHandle.hpp
@@ -15,9 +15,6 @@ namespace HazardSystem {
     template <NonFloatArithmetic Index, typename T>
     struct HazardHandle {
         //--------------------------------------------------------------
-        HazardHandle(void)  = default;
-        ~HazardHandle(void) = default;
-        //--------------------------
         HazardHandle(const Index& index_, std::shared_ptr<T> p_data_) : index(index_), sp_data(std::move(p_data_)) {
             //--------------------------
         }// end HazardHandle(const Index& index_, std::shared_ptr<T> p_data_)
@@ -26,18 +23,26 @@ namespace HazardSystem {
             //--------------------------
         }// end HazardHandle(const std::optional<Index>& index_, std::shared_ptr<T> p_data_)
         //--------------------------
+        HazardHandle(void)                              = default;
+        ~HazardHandle(void)                             = default;
+        //--------------------------
+        HazardHandle(const HazardHandle&)               = delete;
+        HazardHandle& operator=(const HazardHandle&)    = delete;
+        HazardHandle(HazardHandle&&)                    = default;
+        HazardHandle& operator=(HazardHandle&&)         = default;
+        //--------------------------
         bool valid(void) const {
             return sp_data and index.has_value();
         }// end bool valid(void) cons
         //--------------------------
-        void reset(void) {
-            index = std::nullopt;
-            sp_data = nullptr;
-        }// end void reset(void)
+        // void reset(void) {
+        //     index   = std::nullopt;
+        //     sp_data = nullptr;
+        // }// end void reset(void)
         //--------------------------
         std::optional<Index> index;
         std::shared_ptr<T> sp_data;
-        //--------------------------
+        //--------------------------------------------------------------
     };// end struct HazardHandle
     //--------------------------------------------------------------
 } // end namespace HazardSystem
